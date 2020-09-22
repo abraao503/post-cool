30f5d89cf9abdb7309707e82d56da15cd56fd77a
jessikaaguiar <jessika.developer@gmail.com>
2020-09-22 11:32:30 -0400

    pagina e rota para criar o post criados

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CreateAndEditPost from '../views/cadastrar/CreateAndEditPost.vue'
 
 Vue.use(VueRouter)
 
@@ -16,8 +17,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/publico/About.vue')
+  },
+  {
+    path: '/createandeditpost',
+    name: 'CreateAndEditPost',
+    component: CreateAndEditPost
   }
+  
 ]
 
 const router = new VueRouter({
---
+++ src/views/cadastrar/CreateAndEditPost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="createandeditpost">
+    <form action="" method="post">
+        <label for="title">Titulo: <input type="text" name="title" id="title"></label>
+        <br>
+        <label for="body">Texto: <textarea name="body" id="body" cols="30" rows="10"></textarea></label>
+
+        <button>Enviar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CreateAndEditPost',
+  components: {
+    
+  }
+}
+</script>
